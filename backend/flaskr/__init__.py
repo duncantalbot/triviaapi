09f11d661002f08bd672bd618cf38ea21878db2a
talbostools <duncantalbot71@gmail.com>
2020-10-24 14:55:25 +1100

    Build

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import random
 
 from models import setup_db, Question, Category
@@ -16,18 +16,32 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  # CORS(app)
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
-
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def retrieve_categories():
+    categories = Category.query.order_by(Category.type).all()
 
+    if len(categories) == 0:
+      abort(404)
 
+    return jsonify({
+      'success': True,
+      'categories': {category.id: category.type for category in categories}
+    })
   '''
   @TODO: 
   Create an endpoint to handle GET requests for questions, 
