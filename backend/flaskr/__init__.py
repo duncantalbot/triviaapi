e2fa84358770523b3ce87e8056d0a0dd3a7a9c14
talbostools <duncantalbot71@gmail.com>
2020-10-24 15:41:11 +1100

    Final Endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -45,14 +45,14 @@ def create_app(test_config=None):
   '''
   @app.route('/categories')
   def retrieve_categories():
-    categories = Category.query.order_by(Category.type).all()
+    categories_list = Category.query.order_by(Category.type).all()
 
-    if len(categories) == 0:
+    if len(categories_list) == 0:
       abort(404)
 
     return jsonify({
       'success': True,
-      'categories': {category.id: category.type for category in categories}
+      'categories': {category.id: category.type for category in categories_list}
     })
   '''
   @TODO: 
@@ -68,20 +68,20 @@ def create_app(test_config=None):
   '''
   @app.route('/questions')
   def retrieve_questions():
-      selection = Question.query.order_by(Question.id).all()
-      current_questions = paginate_questions(request, selection)
+      selection_list = Question.query.order_by(Question.id).all()
+      current_list_questions = paginate_questions(request, selection_list)
 
-      categories = Category.query.order_by(Category.type).all()
+      categories_list = Category.query.order_by(Category.type).all()
 
-      if len(current_questions) == 0:
+      if len(current_list_questions) == 0:
           abort(404)
 
       return jsonify({
-          'success': True,
-          'questions': current_questions,
-          'total_questions': len(selection),
-          'categories': {category.id: category.type for category in categories},
-          'current_category': None
+          'success': True,         
+          'categories': {category.id: category.type for category in categories_list},
+          'current_category': None,
+          'questions': current_list_questions,
+          'total_questions': len(selection_list)
       })
   '''
   @TODO: 
@@ -93,8 +93,8 @@ def create_app(test_config=None):
   @app.route("/questions/<question_id>", methods=['DELETE'])
   def delete_question(question_id):
       try:
-          question = Question.query.get(question_id)
-          question.delete()
+          question_to_delete = Question.query.get(question_id)
+          question_to_delete.delete()
           return jsonify({
               'success': True,
               'deleted': question_id
@@ -115,24 +115,21 @@ def create_app(test_config=None):
   def add_question():
       body = request.get_json()
 
-      if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
+      if not ('category' in body and 'question' in body and 'difficulty' in body and 'answer' in body):
           abort(422)
 
-      new_question = body.get('question')
-      new_answer = body.get('answer')
-      new_difficulty = body.get('difficulty')
-      new_category = body.get('category')
+      question_new = body.get('question')
+      answer_new = body.get('answer')
+      difficulty_new = body.get('difficulty')
+      category_new = body.get('category')
 
       try:
-          question = Question(question=new_question, answer=new_answer,
-                              difficulty=new_difficulty, category=new_category)
+          question = Question(category=category_new, question=question_new, difficulty=difficulty_new, answer=answer_new)
           question.insert()
-
           return jsonify({
               'success': True,
               'created': question.id,
           })
-
       except:
           abort(422)
 
@@ -152,13 +149,13 @@ def create_app(test_config=None):
       search_term = body.get('searchTerm', None)
 
       if search_term:
-          search_results = Question.query.filter(
+          search_results_list = Question.query.filter(
               Question.question.ilike(f'%{search_term}%')).all()
 
           return jsonify({
               'success': True,
               'questions': [question.format() for question in search_results],
-              'total_questions': len(search_results),
+              'total_questions': len(search_results_list),
               'current_category': None
           })
       abort(404)
@@ -171,15 +168,15 @@ def create_app(test_config=None):
   category to be shown. 
   '''
   @app.route('/categories/<int:category_id>/questions', methods=['GET'])
-  def retrieve_questions_by_category(category_id):
+  def retrieve_questions_category(category_id):
       try:
-          questions = Question.query.filter(
+          questions_list = Question.query.filter(
               Question.category == str(category_id)).all()
 
           return jsonify({
               'success': True,
-              'questions': [question.format() for question in questions],
-              'total_questions': len(questions),
+              'questions': [question.format() for question in questions_list],
+              'total_questions': len(questions_list),
               'current_category': category_id
           })
       except:
@@ -198,7 +195,7 @@ def create_app(test_config=None):
   and shown whether they were correct or not. 
   '''
   @app.route('/quizzes', methods=['POST'])
-  def play_quiz():
+  def play_quiz_game():
 
       try:
           body = request.get_json()
@@ -206,22 +203,22 @@ def create_app(test_config=None):
           if not ('quiz_category' in body and 'previous_questions' in body):
               abort(422)
 
-          category = body.get('quiz_category')
-          previous_questions = body.get('previous_questions')
+          category_quiz = body.get('quiz_category')
+          previous_questions_list = body.get('previous_questions')
 
-          if category['type'] == 'click':
-              available_questions = Question.query.filter(
-              Question.id.notin_((previous_questions))).all()
+          if category_quiz['type'] == 'click':
+              available_questions_list = Question.query.filter(
+              Question.id.notin_((previous_questions_list))).all()
           else:
-              available_questions = Question.query.filter_by(
-              category=category['id']).filter(Question.id.notin_((previous_questions))).all()
+              available_questions_list = Question.query.filter_by(
+              category_list=category['id']).filter(Question.id.notin_((previous_questions_list))).all()
 
-          new_question = available_questions[random.randrange(
-              0, len(available_questions))].format() if len(available_questions) > 0 else None
+          question_new = available_questions_list[random.randrange(
+              0, len(available_questions_list))].format() if len(available_questions_list) > 0 else None
 
           return jsonify({
               'success': True,
-              'question': new_question
+              'question': question_new
           })
       except:
           abort(422)
@@ -230,6 +227,15 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False,
+          "error": 400,
+          "message": "bad request"
+      }), 400
+
   @app.errorhandler(404)
   def not_found(error):
       return jsonify({
@@ -244,14 +250,6 @@ def create_app(test_config=None):
           "success": False,
           "error": 422,
           "message": "unprocessable"
-      }), 422
-
-  @app.errorhandler(400)
-  def bad_request(error):
-      return jsonify({
-          "success": False,
-          "error": 400,
-          "message": "bad request"
-      }), 400
+      }), 422 
 
   return app    
